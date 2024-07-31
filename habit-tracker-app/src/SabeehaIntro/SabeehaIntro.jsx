cbcb9419fde56a45713f51bcc002d81979130a92
SP208 <patels208208@gmail.com>
2024-08-01 00:44:28 +0100

    Sabeeha app.js assignment submission

---
+++ habit-tracker-app/src/SabeehaIntro/SabeehaIntro.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './SabeehaIntro.css';
+
+function SabeehaIntro() {
+  return (
+    <div className="sabeeha-intro">
+      <h2>Sabeeha</h2>
+      <h3>About Me!</h3>
+      <p>
+        Hello, I'm Sabeeha. I'm a pharmacist by background and I'm based in London.
+      </p>
+      <p>
+        I love reading and run a blog which is dedicated to all things book-related in my spare time: themodestbookshelf.wordpress.com.
+      </p>
+      <p>
+        I've been learning to speak Arabic for the last few months.
+      </p>
+      <p>
+        I've been learning to code for about a year now and have really enjoyed web development (HTML, CSS, JavaScript) in particular.
+      </p>
+      <p className="emphasis">
+        I'm looking forward to working on this project and learning how a functioning full-stack app is designed and built from scratch!
+      </p>
+    </div>
+  );
+}
+export default SabeehaIntro;
