ebbce8684b3b45e90c3e9c3f10a6d1b09816cf69
Meghana2119 <meghana_nikam@yahoo.co.uk>
2024-08-01 18:58:06 +0100

    a tag removed

--- habit-tracker-app/src/Meg/MegIntro.jsx
+++ habit-tracker-app/src/Meg/MegIntro.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 export default function MegIntro() {
   return (
     <div>
-      <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Introduction</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Meghana(Meg)</h6>
-    <p class="card-text">I am a Maths teacher. My hobbies are dancing and Trekking.</p>
-    <Link to="/" class="card-link">Read More</Link>
-    
-  </div>
-</div>
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">Introduction</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">Meghana (Meg)</h6>
+          <p className="card-text">I am a Maths teacher. My hobbies are dancing and trekking.</p>
+          <Link to="/" className="card-link">Read More</Link>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
+
