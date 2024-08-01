218236a72bd561dda9983984e82ce315962d03bd
Meghana2119 <meghana_nikam@yahoo.co.uk>
2024-08-01 20:02:15 +0100

    added css

--- habit-tracker-app/src/Meg/MegIntro.jsx
+++ habit-tracker-app/src/Meg/MegIntro.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import './MegIntro.css'
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 export default function MegIntro() {
   return (
-    <div>
-      <div className="card" style={{ width: '18rem' }}>
-        <div className="card-body">
-          <h5 className="card-title">Introduction</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">Meghana (Meg)</h6>
-          <p className="card-text">I am a Maths teacher. My hobbies are dancing and trekking.</p>
-          <Link to="/" className="card-link">Read More</Link>
+    <div className="container">
+            <div className="intro-card">
+                <img src="profile.jpg" alt="Profile" className="profile-img" />
+                <h1 className="name">Meghana (Meg)</h1>
+                <p className="background">I was a software developer with a passion for creating innovative solutions. My background includes a degree in Computer Science.Currently, I am a math teacher.</p>
+                <h2 className="hobbies-heading">Hobbies</h2>
+                <ul className="hobbies-list">
+                    <li>Traveling</li>
+                    <li>Dancing</li>
+                    <li>Watching tik tok videos</li>
+                </ul>
+            </div>
         </div>
-      </div>
-    </div>
   );
 }
 
